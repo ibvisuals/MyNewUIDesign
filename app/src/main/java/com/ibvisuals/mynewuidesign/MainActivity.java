4930fee01285a20ad65725422cd353e1f31e4a4f
Intyaud Banton <65741007+ibvisuals@users.noreply.github.com>
2021-03-01 00:21:38 -0500

    Initial commit

---
+++ app/src/main/java/com/ibvisuals/mynewuidesign/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ibvisuals.mynewuidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doImage(View view) {
+        Toast.makeText(this,"Image Button Clicked",Toast.LENGTH_SHORT).show();
+    }
+}
